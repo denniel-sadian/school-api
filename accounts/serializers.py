239dd3b8c656cfb9cb168734d9b49451e3a401ea
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 21:09:03 +0800

    Add this serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import Employee
+
 
 class UserEmployeeSerializer(serializers.Serializer):
     """
@@ -17,6 +19,13 @@ class UserEmployeeSerializer(serializers.Serializer):
     id_number = serializers.CharField()
     department = serializers.IntegerField()
     photo = serializers.ImageField()
+
+
+class EmployeeProfileSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Employee
+        fields = '__all__'
 
 
 class LoginSerializer(serializers.Serializer):
