5afa0618e799957f95e87d94c094e7b995f830a1
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 15:47:54 +0800

    Correct these

--- information/models.py
+++ information/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class Department(models.Model):
-    name = models.CharField(max_length='50')
+    name = models.CharField(max_length=50)
 
 
 class Section(models.Model):
-    name = models.CharField(max_length='50')
+    name = models.CharField(max_length=50)
 
 
 class Subject(models.Model):
-    name = models.CharField(max_length='50')
+    name = models.CharField(max_length=50)
 
 
 class GuardianViewingPermission(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
-    code = models.CharField(max_length='100')
+    code = models.CharField(max_length=100)
 
 
 class Student(models.Model):
@@ -39,13 +39,13 @@ class Student(models.Model):
         ('c4', 'Fourth Year College')
     )
 
-    firstname = models.CharField(max_length='50')
-    lastname = models.CharField(max_length='50')
-    id_number = models.CharField(max_length='100')
-    cp_number = models.CharField(max_length='12')
-    guardian_cp_number = models.CharField(max_length='12')
-    address = models.CharField(max_length='255')
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    id_number = models.CharField(max_length=100)
+    cp_number = models.CharField(max_length=12)
+    guardian_cp_number = models.CharField(max_length=12)
+    address = models.CharField(max_length=255)
     photo = models.ImageField()
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    grade_level = models.CharField(max_length='2', choices=LEVELS)
+    grade_level = models.CharField(max_length=2, choices=LEVELS)
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
