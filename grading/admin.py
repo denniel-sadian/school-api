6eed7f5923abddf74f34ed1d7ad066f5762de562
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 17:24:29 +0800

    Add some admin stuff

--- grading/admin.py
+++ grading/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.GradingSheet)
-admin.site.register(models.Work)
+
+class GradingSheetAdmin(admin.ModelAdmin):
+    list_display = ('date', 'teacher', 'department', 'subject', 'publish')
+
+
+class WorkAdmin(admin.ModelAdmin):
+    list_display = ('date', 'gsheet', 'work_type', 'highest_score')
+
+
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('date', 'gsheet', 'student', 'work', 'score')
+
+
+admin.site.register(models.GradingSheet, GradingSheetAdmin)
+admin.site.register(models.Work, WorkAdmin)
 admin.site.register(models.Record)
