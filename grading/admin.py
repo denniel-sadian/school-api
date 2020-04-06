dfe9a6c5dfc641b30d04bd09250ffedd41c4dfaf
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 18:08:25 +0800

    Add these

--- grading/admin.py
+++ grading/admin.py
@@ -3,8 +3,14 @@ from django.contrib import admin
 from . import models
 
 
+class WorkInline(admin.StackedInline):
+    model = models.Work
+    extra = 0
+
+
 class GradingSheetAdmin(admin.ModelAdmin):
     list_display = ('date', 'teacher', 'department', 'subject', 'publish')
+    inlines = [WorkInline]
 
 
 class WorkAdmin(admin.ModelAdmin):
