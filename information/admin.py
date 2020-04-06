cebc802a442d7fac7478f6c0a35ab376f07393b6
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 17:31:10 +0800

    Add some admin stuff here

--- information/admin.py
+++ information/admin.py
@@ -2,8 +2,19 @@ from django.contrib import admin
 
 from . import models
 
+
+class GuardianViewingPermissionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'section', 'code')
+
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'id_number', 'cp_number',
+                    'guardian_cp_number', 'address', 'department', 'grade_level',
+                    'section')
+
+
 admin.site.register(models.Department)
 admin.site.register(models.Section)
 admin.site.register(models.Subject)
-admin.site.register(models.GuardianViewingPermission)
-admin.site.register(models.Student)
+admin.site.register(models.GuardianViewingPermission, GuardianViewingPermissionAdmin)
+admin.site.register(models.Student, StudentAdmin)
