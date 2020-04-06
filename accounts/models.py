89442577e817fcfd79be615c234b2b1fc893f998
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 16:56:08 +0800

    Create this new model

--- accounts/models.py
+++ accounts/models.py
@@ -3,15 +3,23 @@ from django.contrib.auth.models import User
 
 from information.models import Department
 
-
-class Employee(models.Model):
-    ROLES = (
+ROLES = (
         ('admin', 'Admin'),
         ('teacher', 'Teacher')
     )
 
+
+class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     id_number = models.CharField(max_length=255)
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     role = models.CharField(max_length=7, choices=ROLES)
     photo = models.ImageField()
+
+
+class EmployeeUserCreationInvitation(models.Model):
+    date = models.DateField(auto_now_add=True)
+    role = models.CharField(max_length=7, choices=ROLES)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    code = models.CharField(max_length=50)
