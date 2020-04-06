8dda41b4934f4652d83bfc05962512376007bdf6
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-06 16:32:57 +0800

    Add this Employer profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from information.models import Department
+
+
+class Employee(models.Model):
+    ROLES = (
+        'admin', 'Admin',
+        'teacher', 'Teacher'
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id_number = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.CharField(max_length=7, choices=ROLES)
+    photo = models.ImageField()
