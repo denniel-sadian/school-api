b4c279d8ea0b8cc766b32fc04d77510066e4c2a3
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-07 22:01:47 +0800

    Comment

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -5,17 +5,27 @@ from .models import ProfileUserCreationPermission
 
 
 class IsAdminOrInvited(BasePermission):
+    """
+    Permission for registering users.
+    """
     message = "You're not an admin or your code was not right."
 
     def has_permission(self, request, view):
+        
+        # Allow these methods
         first = (
             request.method in (SAFE_METHODS + ('POST',))
         )
+        
+        # Allow if the user is an admin
         second = request.user and request.user.is_staff
+        
+        # Allow if permitted
         if 'code' in request.data and not second:
             code = request.data['code']
             there_is = ProfileUserCreationPermission.objects.filter(code=code).exists()
             return there_is
+        
         return bool(first or second)
 
 
