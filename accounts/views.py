d8ff1b4a54813c1b4771e61ac3de5c0097f83edb
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-07 10:57:41 +0800

    Add this

--- accounts/views.py
+++ accounts/views.py
@@ -8,12 +8,39 @@ from rest_framework import status
 from information.models import Department
 from .serializers import UserEmployeeSerializer
 from .serializers import EmployeeProfileSerializer
+from .serializers import LoginSerializer
 from .models import Employee
 
 
 class EmployeeProfileViewSet(ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeProfileSerializer
+
+
+class LoginView(GenericAPIView):
+    """
+    View for logging user in.
+    """
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        data = serializer.data
+        user = authenticate(username=data['username'],
+                            password=data['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return Response({
+                    "token": user.auth_token.key,
+                    "username": user.username,
+                    "first_name": user.first_name,
+                    "last_name": user.last_name,
+                }, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Wrong Credentials"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
 
 class CreateUserEmployeeView(GenericAPIView):
