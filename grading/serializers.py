8d4c8565148ce89ca8e68840ab45eca544250db6
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 19:43:24 +0800

    Add some more fields

--- grading/serializers.py
+++ grading/serializers.py
@@ -10,21 +10,31 @@ class RecordSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Record
         fields = '__all__'
+        extra_kwargs = {
+            'date': {'read_only': True}
+        }
 
 
 class WorkSerializer(serializers.HyperlinkedModelSerializer):
+    records = RecordSerializer(many=True, read_only=True)
 
     class Meta:
         model = Work
-        fields = '__all__'
+        fields = ('url', 'name', 'gsheet', 'work_type', 'highest_score', 'records',
+                  'date')
+        extra_kwargs = {
+            'date': {'read_only': True}
+        }
 
 
 class GradingSheetSerializer(serializers.HyperlinkedModelSerializer):
+    works = WorkSerializer(many=True, read_only=True)
+    records = RecordsSerializer(many=True, read_only=True)
 
     class Meta:
         model = GradingSheet
         fields = ('url', 'teacher', 'subject', 'department',
-                  'section', 'date', 'publish')
+                  'section', 'date', 'publish', 'works', 'records')
         extra_kwargs = {
             'date': {'read_only': True}
         }
