3bb401e315e6a99b3109aa6d5b42be35dc925360
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 22:34:12 +0800

    Add id

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -41,6 +41,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
+            'id',
             'username',
             'email',
             'first_name',
