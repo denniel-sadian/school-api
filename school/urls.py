fc6185fcb3f80d6f5c901968ec57faf91f6de15d
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 09:11:43 +0800

    Remove this

--- school/urls.py
+++ school/urls.py
@@ -22,7 +22,7 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls'))
 ]
 
 if settings.DEBUG:
