7d557121aa14ecb2219e8b3ca1a5fd8294cd882f
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 10:06:18 +0800

    Add the permission

--- information/views.py
+++ information/views.py
@@ -10,26 +10,31 @@ from .serializers import SectionSerializer
 from .serializers import SubjectSerializer
 from .serializers import GuardianViewingPermissionSerializer
 from .serializers import StudentSerializer
+from .permissions import IsAdminOrReadOnly
 
 
 class DepartmentViewSet(ModelViewSet):
     queryset = Department.objects.all()
     serializer_class = DepartmentSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class SectionViewSet(ModelViewSet):
     queryset = Section.objects.all()
     serializer_class = SectionSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class SubjectViewSet(ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class GuardianViewingPermissionViewSet(ModelViewSet):
     queryset = GuardianViewingPermission.objects.all()
     serializer_class = GuardianViewingPermissionSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class StudentViewSet(ModelViewSet):
