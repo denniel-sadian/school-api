ba4b55fda4f5c76598e67c7b8484aefff3ea38e9
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 17:19:14 +0800

    Add this meta class

--- information/models.py
+++ information/models.py
@@ -35,6 +35,9 @@ class GuardianViewingPermission(models.Model):
     section = models.ForeignKey(Section, on_delete=models.CASCADE)
     code = models.CharField(max_length=100)
 
+    class Meta:
+        unique_together = ('code', 'section')
+
     def __str__(self):
         return self.code
 
