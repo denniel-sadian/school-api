058980071aff769139be2463e3ecc73cf945afca
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 21:32:51 +0800

    Fix this permission

--- grading/permissions.py
+++ grading/permissions.py
@@ -1,12 +1,25 @@
 from rest_framework.permissions import BasePermission
 from rest_framework.permissions import SAFE_METHODS
 
+from .models import Work
+from .models import Record
+
 
 class IsTeacherAndOwnerOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        # Allow if read-only and authenticated or a teacher
+        authenticated = request.user.is_authenticated
+        is_teacher = not request.user.is_staff
+        if request.method in SAFE_METHODS and authenticated or is_teacher:
+            return True
+        return False
     
     def has_object_permission(self, request, view, obj):
-        return False
-        print('TYPE: ', type(obj))
-        if not request.user.is_staff:
-            return False
-        return request.user == obj.teacher 
+        # Allow authenticated users to view
+        if request.method in SAFE_METHODS and request.user.is_authenticated:
+            return True
+        # Allow only the owners
+        if type(obj) in (Work, Record):
+            return obj.gsheet.teacher == obj.teacher
+        return request.user == obj.teacher
