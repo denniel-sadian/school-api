5b63cbfb62d4b5a025fa76b230e887528e78e423
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 14:21:49 +0800

    Add these

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from . import views
 
 router = DefaultRouter()
+router.register('users', views.UserViewSet)
 router.register('permissions', views.AccountCreationPermissionViewSet)
 
 urlpatterns = [
--- accounts/views.py
+++ accounts/views.py
@@ -7,6 +7,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework import status
 
 from information.models import Department
@@ -34,6 +35,7 @@ def log_out(request):
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = IsAuthenticatedOrReadOnly
 
 
 class LoginView(GenericAPIView):
