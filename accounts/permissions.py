5450db6715333ce2faf8e450fd597bb5b2c48c21
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 11:17:29 +0800

    Delete useless permission

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -27,11 +27,3 @@ class IsAdminOrInvited(BasePermission):
             return there_is
         
         return bool(first or second)
-
-
-class IsOwnerOrReadOnly(BasePermission):
-
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.from_who == request.user
