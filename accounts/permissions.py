b0ff26f29f9bc180779d446b739a1ec6f5401afc
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 20:05:25 +0800

    Update this

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -34,6 +34,9 @@ class IsOwnerOrReadOnly(BasePermission):
     Custom permission to only allow owners of an object to edit it.
     """
 
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_staff)
+
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
