9957932cd3407d21f507ad1bc897deb15f7dad30
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 20:32:41 +0800

    Add this viewset

--- grading/views.py
+++ grading/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import GradingSheet
+from .models import Work
+from .models import Record
+from .serializers import GradingSheetSerializer
+from .serializers import WorkSerializer
+from .serializers import RecordSerializer
+from .permissions import IsTeacherAndOwnerOrReadOnly
+
+
+class GradingSheetViewSet(ModelViewSet):
+    queryset = GradingSheet.objects.all()
+    serializer_class = GradingSheetSerializer
+    permission_classes = (IsTeacherAndOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(teacher=self.request.user)
