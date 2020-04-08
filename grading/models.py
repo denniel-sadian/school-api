830cccbf2b8628969de1829c781d64927313a811
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 21:02:17 +0800

    Update

--- grading/models.py
+++ grading/models.py
@@ -9,7 +9,7 @@ from information.models import Department
 
 class GradingSheet(models.Model):
     date = models.DateField(auto_now_add=True)
-    teacher = models.ForeignKey(Profile, on_delete=models.CASCADE,
+    teacher = models.ForeignKey('auth.user', on_delete=models.CASCADE,
                                 related_name='grading_sheets')
     department = models.ForeignKey(Department, on_delete=models.CASCADE,
                                    related_name='grading_sheets')
