785ba7f338fcc13a2712fcb62cc1f68e5155ff92
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 09:54:07 +0800

    Make the routes

---
+++ information/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register('departments', views.DepartmentViewSet)
+router.register('sections', views.SectionViewSet)
+router.register('subjects', views.SubjectViewSet)
+router.register('viewing-permissions', views.GuardianViewingPermissionViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
