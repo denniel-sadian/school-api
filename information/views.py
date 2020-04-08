1029c4aa5463d0b4807c2570a229834a3bfde9a4
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 09:51:15 +0800

    Make the viewsets

--- information/views.py
+++ information/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Department
+from .models import Section
+from .models import Subject
+from .models import GuardianViewingPermission
+from .models import Student
+from .serializers import DepartmentSerializer
+from .serializers import SectionSerializer
+from .serializers import SubjectSerializer
+from .serializers import GuardianViewingPermissionSerializer
+from .serializers import StudentSerializer
+
+
+class DepartmentViewSet(ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class SectionViewSet(ModelViewSet):
+    queryset = Section.objects.all()
+    serializer_class = SectionSerializer
+
+
+class SubjectViewSet(ModelViewSet):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+
+class GuardianViewingPermissionViewSet(ModelViewSet):
+    queryset = GuardianViewingPermission.objects.all()
+    serializer_class = GuardianViewingPermissionSerializer
+
+
+class StudentViewSet(ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
