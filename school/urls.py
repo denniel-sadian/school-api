9f9c4d64a759c5386c0ee6a031713f250babdb16
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-08 20:38:02 +0800

    Add this to the root

--- school/urls.py
+++ school/urls.py
@@ -23,6 +23,7 @@ from django.views.static import serve
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    path('grading/', include('grading.urls')),
     path('information/', include('information.urls')),
 ]
 
