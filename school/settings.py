d1e8b6aa5b96538d57e8f1fc91753b26b08e1e1c
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-04-12 11:33:36 +0800

    Add cors

--- school/settings.py
+++ school/settings.py
@@ -106,6 +106,21 @@ REST_FRAMEWORK = {
 }
 
 
+# CORS
+CORS_ORIGIN_ALLOW_ALL = True
+"""
+CORS_ORIGIN_WHITELIST = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000"
+]
+CSRF_TRUSTED_ORIGINS = [
+    'change.allowed.com',
+]
+"""
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
