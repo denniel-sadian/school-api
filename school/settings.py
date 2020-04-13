f11650f05fe580dc5158808fab6d5a4dd016ca57
Denniel Sadian <sadiandenniel@gmail.com>
2020-04-13 23:49:49 +0800

    Update settings.py

--- school/settings.py
+++ school/settings.py
@@ -57,11 +57,11 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+# 'django.middleware.csrf.CsrfViewMiddleware'
 
 ROOT_URLCONF = 'school.urls'
 
