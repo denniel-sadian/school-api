6a614519b74ae9e689e3a12ceda5449bbc136baa
Denniel <sadiandenniel@gmail.com>
2020-04-15 22:17:06 +0800

    Ok

--- school/settings.py
+++ school/settings.py
@@ -177,7 +177,9 @@ MEDIA_URL = '/media/'
 
 # GITHUB S3
 
-DEFAULT_FILE_STORAGE = 'school.backend.TheStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'school.backend.TheStorage'
 GITHUB_HANDLE = 'denniel-sadian'
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 GITHUB_REPO_NAME = os.getenv('GITHUB_REPO_NAME', 'school-bucket')
