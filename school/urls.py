5bc852fe3d71b17ed29dae0a9dea5f3a8ce92a5d
Denniel <sadiandenniel@gmail.com>
2020-04-15 22:31:32 +0800

    Remove these

--- school/urls.py
+++ school/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
-from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
 
@@ -26,6 +25,4 @@ urlpatterns = [
     path('grading/', include('grading.urls')),
     path('information/', include('information.urls')),
 ]
-
-if settings.DEBUG:
-     urlpatterns += url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+urlpatterns += url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
