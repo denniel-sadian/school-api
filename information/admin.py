33d1837c30fcc00df5d0a8849f98574a27221054
Dein <sadiandenniel@gmail.com>
2020-04-21 18:41:20 +0800

    Remove this

--- information/admin.py
+++ information/admin.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 from . import models
 
 
-class GuardianViewingPermissionAdmin(admin.ModelAdmin):
-    list_display = ('date', 'section', 'code')
-
-
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'id_number', 'cp_number',
                     'guardian_cp_number', 'address', 'department', 'grade_level',
@@ -16,5 +12,4 @@ class StudentAdmin(admin.ModelAdmin):
 admin.site.register(models.Department)
 admin.site.register(models.Section)
 admin.site.register(models.Subject)
-admin.site.register(models.GuardianViewingPermission, GuardianViewingPermissionAdmin)
 admin.site.register(models.Student, StudentAdmin)
