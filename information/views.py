9952c972a115393c9d8fe215c97ee5fa601ec304
Dein <sadiandenniel@gmail.com>
2020-04-21 18:42:01 +0800

    Remove these

--- information/views.py
+++ information/views.py
@@ -3,12 +3,10 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Department
 from .models import Section
 from .models import Subject
-from .models import GuardianViewingPermission
 from .models import Student
 from .serializers import DepartmentSerializer
 from .serializers import SectionSerializer
 from .serializers import SubjectSerializer
-from .serializers import GuardianViewingPermissionSerializer
 from .serializers import StudentSerializer
 from .permissions import IsAuthenticatedOrAdmin
 
@@ -28,15 +26,6 @@ class SectionViewSet(ModelViewSet):
 class SubjectViewSet(ModelViewSet):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
-
-
-class GuardianViewingPermissionViewSet(ModelViewSet):
-    queryset = GuardianViewingPermission.objects.all()
-    serializer_class = GuardianViewingPermissionSerializer
-    permission_classes = (IsAuthenticatedOrAdmin,)
-
-    def perform_create(self, serializer):
-        serializer.save(from_who=self.request.user)
 
 
 class StudentViewSet(ModelViewSet):
