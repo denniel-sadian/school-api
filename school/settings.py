16bcf8409edccece9cf2ec163c01d6ed8d807a8a
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-05-04 12:30:45 +0800

    Modify this list

--- school/settings.py
+++ school/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['school.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    'school.pythonanywhere.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
