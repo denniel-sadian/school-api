3716aafa1c902d7c17b9ddafdc0078f3db05fe0c
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-05-04 14:08:56 +0800

    Add this `on_delete` attr

--- information/models.py
+++ information/models.py
@@ -54,7 +54,7 @@ class Student(models.Model):
         ('f', 'Female')
     )
 
-    user = models.OneToOneField('auth.user', null=True)
+    user = models.OneToOneField('auth.user', null=True, on_delete=models.SET_NULL)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     gender = models.CharField(max_length=1, choices=GENDERS)
