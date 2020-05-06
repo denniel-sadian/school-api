092fa0c79abd0513eab16b096a83783d46fd088f
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-05-06 19:59:50 +0800

    Fix these imports

--- exam/admin.py
+++ exam/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-from models import Exam
-from models import Item
-from models import Choice
-from models import Session
+from .models import Exam
+from .models import Item
+from .models import Choice
+from .models import Session
 
 admin.site.register(Exam)
 admin.site.register(Item)
