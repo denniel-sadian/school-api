d77117a3ff0c33085b8ef52e3d3582a5193aff62
Dein <sadiandenniel@gmail.com>
2020-05-06 14:15:24 +0800

    Add a new route

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('logout/', views.log_out, name='logout'),
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDeleteView.as_view(), name='user-delete'),
+    path('user-detail/<int:pk>/', views.UserDeleteView.as_view(), name='user-detail'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('register/', views.CreateUserProfileView.as_view(), name='register'),
     path('change-password/', views.ChangePasswordView.as_view(),
