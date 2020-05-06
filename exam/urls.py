7a959b69fff8d94cc6137a18d546074669739afd
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-05-06 20:36:04 +0800

    Add this

--- exam/urls.py
+++ exam/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 router = DefaultRouter()
 router.register('exams', views.ExamViewSet)
+router.register('items', views.ItemViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
