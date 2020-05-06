43b8121506755f4a5a0fc9b22b6f211004ae72e3
Denniel Luis Sadian <sadiandenniel@gmail.com>
2020-05-06 22:50:56 +0800

    Put this back

--- school/settings.py
+++ school/settings.py
@@ -59,12 +59,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    
+    'django.middleware.csrf.CsrfViewMiddleware',   
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# 'django.middleware.csrf.CsrfViewMiddleware',
 
 ROOT_URLCONF = 'school.urls'
 
