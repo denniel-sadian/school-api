1c0890a45570d764c2e7b666548c96dedfd0f9d3
Dein <sadiandenniel@gmail.com>
2020-05-10 20:05:44 +0800

    Update these methods

--- exam/views.py
+++ exam/views.py
@@ -26,10 +26,14 @@ class ExamViewSet(ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(teacher=self.request.user)
         for sheet in serializer.instance.sheets.all():
+            sheet.has_multiple_choice_exam = True
+            sheet.save()
             sheet.works.create(work_type='e', name='Examination')
     
     def perform_destroy(self, instance):
         for sheet in instance.sheets.all():
+            sheet.has_multiple_choice_exam = True
+            sheet.save()
             sheet.works.filter(work_type='e').delete()
         instance.delete()
 
