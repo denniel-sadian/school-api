215521e1168be3a98f7eb4a0e37a2695a95c3caa
Dein <sadiandenniel@gmail.com>
2020-05-11 12:27:35 +0800

    Register this new model

--- exam/admin.py
+++ exam/admin.py
@@ -4,8 +4,10 @@ from .models import Exam
 from .models import Item
 from .models import Choice
 from .models import Session
+from .models import AdminComment
 
 admin.site.register(Exam)
 admin.site.register(Item)
 admin.site.register(Choice)
 admin.site.register(Session)
+admin.site.register(AdminComment)
