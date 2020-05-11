f21420182e7d23488232fa6406d1d4a6adf8c107
Dein <sadiandenniel@gmail.com>
2020-05-11 14:44:54 +0800

    Add this field here

--- exam/serializers.py
+++ exam/serializers.py
@@ -36,7 +36,8 @@ class ExamSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Exam
-        fields = ('url', 'id', 'teacher', 'date', 'published', 'sheets', 'items')
+        fields = ('url', 'id', 'teacher', 'date', 'published', 'sheets', 'items',
+                  'comments')
         extra_kwargs = {
             'date': {'read_only': True},
             'teacher': {'view_name': 'user-detail', 'read_only': True},
