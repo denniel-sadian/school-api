688ea74bb19df03a7b5905bee8705f9add9ab388
Dein <sadiandenniel@gmail.com>
2020-05-11 21:20:53 +0800

    Change the comments

--- exam/admin.py
+++ exam/admin.py
@@ -4,10 +4,10 @@ from .models import Exam
 from .models import Item
 from .models import Choice
 from .models import Session
-from .models import AdminComment
+from .models import StaffComment
 
 admin.site.register(Exam)
 admin.site.register(Item)
 admin.site.register(Choice)
 admin.site.register(Session)
-admin.site.register(AdminComment)
+admin.site.register(StaffComment)
