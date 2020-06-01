bff91228d5829c6a0524e0d2ea1fcb5a2edd30e4
Dein <sadiandenniel@gmail.com>
2020-06-01 21:33:24 +0800

    Remove this import

--- grading/views.py
+++ grading/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.forms.models import model_to_dict
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
